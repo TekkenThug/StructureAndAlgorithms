20b85b334e7c5a94d246fde472c9b3c1360447b3
Vadim-Matvei-Notebook <ignatov0131@gmail.com>
2021-03-31 21:33:26 +0300

    Добавление бинарного дерева и задачи про ферзей

--- LR№2/script.js
+++ LR№2/script.js
@@ -2,7 +2,7 @@
 /* Реализовать функции поиска */
 
 /* Случайные данные */
-let data = randomData(25);
+let data = randomData(10);
 function randomData(n = 10, minLim = 0, maxLim = 10) {
   let data = [];
 
@@ -39,6 +39,196 @@ function binarySearch(value, list) {
 }
 
 /* Бинарное дерево */
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+    this.size = 0;
+  }
+  add(data) {
+    const newNode = new Node(data);
+    if (this.root === null) {
+      this.root = newNode;
+      this.size++;
+    }
+    let current = this.root;
+    while (true) {
+      if (data < current.data) {
+        if (current.left === null) {
+          current.left = newNode;
+          this.size++;
+          break;
+        } else {
+          current = current.left;
+        }
+      } else if (data > current.data) {
+        if (current.right === null) {
+          current.right = newNode;
+          this.size++;
+          break;
+        } else {
+          current = current.right;
+        }
+      } else {
+        break;
+      }
+    }
+  }
+  /**
+   * Максимальный узел в дереве
+   * @memberof BinaryTree
+   */
+  getMax() {
+    let current = this.root;
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
+  }
+  /**
+   * Минимальный узел в дереве
+   * @memberof BinaryTree
+   */
+  getMin() {
+    let current = this.root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
+  }
+  /**
+   * Количество узлов в дереве
+   * @memberof BinaryTree
+   */
+  size() {
+    return this.size;
+  }
+  find(data) {
+    let current = this.root;
+    while (current.data !== data) {
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+      if (current === null) {
+        return false;
+      }
+    }
+    return true;
+  }
+  /**
+   * Прямой обход дерева
+   * @param {any} node
+   * @memberof BinaryTree
+   */
+  preOrder(node) {
+    if (node === null) {
+      return;
+    }
+    console.log(node.data);
+    this.preOrder(node.left);
+    this.preOrder(node.right);
+  }
+  /**
+   * Симметричный обход дерева
+   * @param {any} node
+   * @memberof BinaryTree
+   */
+  inOrder(node) {
+    if (node === null) {
+      return;
+    }
+    this.inOrder(node);
+    console.log(node.data);
+    this.inOrder(node);
+  }
+  /**
+   * Обратный обход дерева
+   * @param {any} node
+   * @memberof BinaryTree
+   */
+  postOrder(node) {
+    if (node === null) {
+      return;
+    }
+    this.inOrder(node);
+    this.inOrder(node);
+    console.log(node.data);
+  }
+  /**
+   * Обход дерева в ширину
+   * @param {any} node
+   * @memberof BinaryTree
+   */
+  bfs(node) {
+    let queue = [];
+    let values = [];
+    queue.push(node);
+    while (queue.length > 0) {
+      let current = queue.shift();
+      values.push(current.data);
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+    }
+    return values;
+  }
+  remove(data) {
+    const removeNode = function (node, data) {
+      if (node == null) {
+        return null;
+      }
+      if (data == node.data) {
+        // У узла нет детей
+        if (node.left === null && node.right === null) {
+          return null;
+        }
+        // У узла только правый ребенок
+        if (node.left === null) {
+          return node.right;
+        }
+        // У узла только левый ребенок
+        if (node.right === null) {
+          return node.left;
+        }
+        // У узла двое детей
+        var current = node.right;
+        while (current.left !== null) {
+          current = current.left;
+        }
+        node.data = current.data;
+        node.right = removeNode(node.right, current.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this.root = removeNode(this.root, data);
+  }
+}
+
+let tree = new BinaryTree();
+
+data.forEach((item) => {
+  tree.add(item);
+});
+
+// console.log(tree);
 
 /* Фиббоначиев поиск */
 function fibMonaccianSearch(x, arr) {
@@ -149,51 +339,83 @@ function dataProcess(originalData, value, func, signature) {
 один из них не находился под боем другого». Подразумевается, что ферзь бьёт все клетки,
 расположенные по вертикалям, горизонталям и обеим диагоналям
 Написать программу, которая находит хотя бы один способ решения задач. */
-let solutions = [];
+const OCCUPIED = 1, //метка "поле бьётся"
+  FREE = 0, //метка "поле не бьётся"
+  ISHERE = -1; //метка "ферзь тут"
 
-function queen() {
-  let columns = [];
-  let numberOfDiagonals = 15;
-  let diagonals1;
-  let diagonals2;
-  
-  for (let i = 0; i < numberOfDiagonals; i++) {
-    if (i < 8) columns[i] = -1;
+class Queen {
+  constructor(N) {
+    this.N = N;
 
-    diagonals1[i] = diagonals2 = 0;
+    for (let i = 0; i < 2 * this.N - 1; i++) {
+      if (i < this.N) this.columns[i] = ISHERE;
+
+      this.diagonals1[i] = FREE;
+      this.diagonals2[i] = FREE;
+    }
+  }
+
+  columns = [];
+  solutions = [];
+  diagonals1 = [];
+  diagonals2 = [];
+
+  run(row = 0) {
+    for (let column = 0; column < this.N; ++column) {
+      if (this.columns[column] >= 0) {
+        //текущий столбец бьётся, продолжить
+        continue;
+      }
+
+      let thisDiag1 = row + column;
+
+      if (this.diagonals1[thisDiag1] == OCCUPIED) {
+        //диагональ '\' для текущих строки и столбца бьётся, продолжить
+        continue;
+      }
+      let thisDiag2 = this.N - 1 - row + column;
+
+      if (this.diagonals2[thisDiag2] == OCCUPIED) {
+        //диагональ '/' для текущих строки и столбца бьётся, продолжить
+        continue;
+      }
+
+      this.columns[column] = row;
+      this.diagonals1[thisDiag1] = OCCUPIED; //занять диагонали, которые теперь бьются
+      this.diagonals2[thisDiag2] = OCCUPIED;
+
+      if (row == this.N - 1) {
+        //найдена последняя строка - есть решение
+        this.solutions.push(this.columns.slice());
+      } else {
+        //иначе рекурсия
+        this.run(row + 1);
+      }
+
+      this.columns[column] = ISHERE;
+      this.diagonals1[thisDiag1] = FREE;
+      this.diagonals2[thisDiag2] = FREE;
+    }
   }
 }
 
-function calculate(row, columns, solutions) {
-  for (let i = 0; i < 8; ++i) {
-    if (columns[i] >= 0) continue;
-
-    let thisDiag1 = row + i;
-
-    if (diagonals1[thisDiag1] == 1) continue;
-
-    let thisDiag2 = 7 - row + i;
-
-    if (diagonals2[thisDiag2] == 1) continue;
-
-    columns[i] = row;
-
-    diagonals1[thisDiag1] = 1;
-    diagonals2[thisDiag2] = 1;
-
-    if (row == 7) solutions.push(columns.slice());
-    else calculate(row + 1);
-
-    columns[i] = -1;
-    diagonals1[thisDiag1] = 0;
-    diagonals2[thisDiag2] = 0;
-  }
+function getLine(solution) {
+  return solution.reduce((previous, current, currentIndex) => {
+    return previous + `(${currentIndex + 1},${current + 1})`;
+  }, "");
 }
 
-function getline(solution) {
-  //сформировать строку с решением
-  var line = "";
-  for (var j = 0; j < solution.length; ++j)
-    line += "(" + (j + 1) + "," + (solution[j] + 1) + ")";
-  return line;
+function queenPositions(N = 8) {
+  let table = new Queen(N);
+
+  console.log(`Размер доски: ${table.N}x${table.N}`);
+  console.time("Время вычисления");
+  table.run();
+
+  console.timeEnd("Время вычисления");
+  console.log(`Количество решений: ${table.solutions.length}`);
+
+  table.solutions.forEach((solution) => console.log(getLine(solution)));
 }
+
+queenPositions();
