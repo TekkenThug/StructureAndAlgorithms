9b2473e2f084ca59eff395b27ca1ab6b00799dba
Vadim <ignatov0131@gmail.com>
2021-05-18 12:56:29 +0300

    Добавление задачи про интервалы

---
+++ Исходный код/Задачи по курсовой/problems-intervals.js
@@ -0,0 +1,30 @@
+const sumIntervals = (intervals) => {
+    /* Сортировка */
+    intervals.sort( (a, b) => {
+        return a[0] - b[0]
+    })
+
+    /* Поиск пересекающихся интервалов */
+    intervals.forEach((firstInterval, firstIndex) => {
+        intervals.forEach((secondInterval, secondIndex) => {
+            if (firstIndex !== secondIndex) {
+                if (secondInterval[0] <= firstInterval[1] && secondInterval[0] >= firstInterval[0]) {
+                    if (secondInterval[1] > firstInterval[1])
+                        firstInterval[1] = secondInterval[1];
+
+                    secondInterval[0] = secondInterval[1] = -1
+                }
+            }
+
+        })
+    });
+
+    /* Удаление отрицательных */
+    intervals = intervals.filter(elem => elem[0] !== -1 && elem[1] !== -1);
+
+    /* Вывод в консоль массива */
+    console.log(intervals)
+}
+
+sumIntervals([[8,10], [1,3], [15,18],[2,6]])
+sumIntervals([[4,5], [1,4]])
