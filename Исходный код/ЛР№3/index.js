65b8e3dc55b289a9aacce6bf736a28270bee0d54
Vadim-K <ignatov0131@gmail.com>
2021-05-20 14:38:57 +0300

    Окончание 3 лабораторной

--- Исходный код/ЛР№3/index.js
+++ Исходный код/ЛР№3/index.js
@@ -17,33 +17,54 @@ const searchSubstr = (elem, str, funcName, signature, register = false) => {
     console.timeEnd(signature)
 }
 
-const KnutMorris = (str, elem) => {
-    let strLength = str.length;
-    let elemLength = elem.length;
+/* M + N сравнений в среднем */
+const KMP = (text, word) => {
+    const prefixFunc = (word) => {
+        const prefixTable = [0];
+        let prefixIndex = 0;
+        let suffixIndex = 1;
 
-    let p = 0;
-    p[0] = 0;
-
-    for (let i = 1, j = 0; i < elemLength; i++) {
-        while (j > 0 && elem[j] !== elem[i]) {
-            j = p[j - 1]
+        while (suffixIndex < word.length) {
+            if (word[prefixIndex] === word[suffixIndex]) {
+                prefixTable[suffixIndex] = prefixIndex + 1;
+                suffixIndex += 1;
+                prefixIndex += 1;
+            } else if (prefixIndex === 0) {
+                prefixTable[suffixIndex] = 0;
+                suffixIndex += 1;
+            } else {
+                prefixIndex = prefixTable[prefixIndex - 1];
+            }
         }
 
-        if (elem[j] === elem[i]) j++
-
-        p[i] = j;
+        return prefixTable;
     }
 
-    for (let i = 0, j = 0; i < strLength; i++) {
-        while (j > 0 && elem[j] !== str[i]) j = p[j - 1]
+    if (word.length === 0) return 0;
 
-        if (elem[j] === str[i]) j++
+    let textIndex = 0;
+    let wordIndex = 0;
 
-        if (j === elemLength) return i - j + 1
+    const prefixArr = prefixFunc(word);
+
+    while (textIndex < text.length) {
+        if (text[textIndex] === word[wordIndex]) {
+            if (wordIndex === word.length - 1) return (textIndex - word.length) + 1
+
+            wordIndex += 1;
+            textIndex += 1;
+        } else if (wordIndex > 0) {
+            wordIndex = prefixArr[wordIndex - 1];
+        } else {
+            wordIndex = 0;
+            textIndex += 1;
+        }
     }
 
-    return -1
+    return -1;
 }
+
+/* N/M в лучших случаях, M + N в среднем */
 const BoyerMour = (str, substr) => {
     let strLen = str.length;
     let substrLen = substr.length;
@@ -89,7 +110,7 @@ function searching() {
     str.indexOf(substr);
     console.timeEnd("Стандартная функция")
 
-    searchSubstr(substr, str, KnutMorris, "Алгоритм Кнута-Морриса-Пратта");
+    searchSubstr(substr, str, KMP, "Алгоритм Кнута-Морриса-Пратта");
     searchSubstr(substr, str, BoyerMour, "Алгоритм Бойера-Мура");
 }
 
@@ -100,56 +121,58 @@ searching();
 то есть можно ли из него за конечное число шагов перейти к правильному.
 Если это возможно, то необходимо найти хотя бы одно решение - последовательность движений,
 после которой числа будут расположены в правильном порядке. */
-/**
- * @param {number[][]} board
- * @return {number}
- */
-var slidingPuzzle = function(board) {
-    // BOARD to STRING
-    const serializePuzzle = (board) => {
-        let str = '';
-        board.forEach(row=>{
-            row.forEach(s=>{
-                str += s;
-            })
-        })
-        return str;
+
+const slidingPuzzle = board => {
+    /*
+    mapping is a dictionnary of possible swapping directions
+      0 1 2 we can swap index 0 only with indices 1 and 3 in the original board
+      3 4 5                                                    
+    */
+    const mapping = {
+        0: [1, 3],
+        1: [0, 2, 4],
+        2: [1, 5],
+        3: [0, 4],
+        4: [1, 3, 5],
+        5: [2, 4]
     }
 
-    const target = "123450";
-    if (serializePuzzle(board) === target) return 0;
-    const swaps = [[1, 3], [0, 2, 4], [1, 5], [0, 4], [1, 3, 5], [2, 4]];
-    let toVisit = [serializePuzzle(board)];
-    let visited = new Set();
-    let result = 0;
+    // a simple function to swap values, it's easier to convert a string to an array then swap
+    const swap = (state, pos, next) => {
+        const array = state.split('');
+        [array[pos], array[next]] = [array[next], array[pos]];
+        return array.join('')
+    }
 
-    while (toVisit.length){
-        // LEVEL TRAVEL
-        let levelSize=toVisit.length;
-        for (let i=0; i<levelSize; i++){
-            let encodedBoard = toVisit.shift();
-            let pos0 = encodedBoard.indexOf("0");
-            visited.add(encodedBoard);
+    // convert board to string Exmpl '123540'
+    let state = '';
+    board.forEach(row => state += row.join(''));
 
-            for (let pos of swaps[pos0]){
-                // SWAP
-                let temp = encodedBoard[pos];
-                let newBoard = encodedBoard.slice().split("");
-                newBoard[pos] = "0";
-                newBoard[pos0] = temp;
-                newBoard = newBoard.join("")
+    const visited = new Set(state);
 
-                // CONCLUDE
-                if (!visited.has(newBoard)){
-                    if (newBoard === target) return result+1;
-                    toVisit.push(newBoard);
-                }
-            }
+    // Queue to keep track for state,  position of 0 and moves so far
+    const q = [[state, state.indexOf('0'), 0]];
+
+    while (q.length) {
+
+        const [state, pos, moves] = q.shift();
+
+        if (state == '123450')
+            return moves;
+
+        // move 0 to the possible directions (next) in mapping dict 
+        for (let next of mapping[pos]) {
+            const newState = swap(state, pos, next);
+
+            if (visited.has(newState))
+                continue;
+
+            visited.add(newState);
+            q.push([newState, next, moves + 1])
         }
-        result++;
     }
-
     return -1;
+
 };
 
-slidingPuzzle([[15, 2, 1, 12],[8, 5, 6, 11], [4, 9, 10, 7], [3, 14, 13]])
+console.log(slidingPuzzle([[1,2,3],[4,0,5]]));
